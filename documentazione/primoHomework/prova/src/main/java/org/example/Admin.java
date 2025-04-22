1e7de0370d0b451a4045d59b1ebad4cc443fd892
Mario-jpeg <m.laurato04@gmail.com>
2025-04-22 14:48:20 +0200

    questo è un inizio del progetto

---
+++ documentazione/primoHomework/prova/src/main/java/org/example/Admin.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Admin extends User{
+    private String opCode;
+
+    public Admin(String username, String password) {
+        super(username, password);
+        this.opCode = opCode;
+    }
+
+    public void insertFlight(){
+
+    }
+
+    public void updateFlight(){
+
+    }
+
+    public void updateGate(){
+
+    }
+
+    public void updateLuggage(){
+
+    }
+
+    public void viewLost(){
+
+    }
+}
