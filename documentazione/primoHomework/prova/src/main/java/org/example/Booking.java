6483ebedeb5fd5bac82b5082ddcda5c17e69cfaf
Mario-jpeg <m.laurato04@gmail.com>
2025-04-22 15:05:12 +0200

    rinominato il progetto e aggiunte minori

---
+++ documentazione/primoHomework/prova/src/main/java/org/example/Booking.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Booking {
+    private String bookingCode;
+    private String ticketNum;
+    private boolean seatAssigned;
+    private BookingStatus status;
+
+    public void checkIn(){
+
+    }
+
+    public void printBooking(){
+
+    }
+}
